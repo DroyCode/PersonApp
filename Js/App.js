b9acd3d8405696958d971f172d03f64f0157841c
Engels José Cordones De Jesus <engels.cordones@gmail.com>
2025-08-04 21:50:34 -0400

    first commit

---
+++ Js/App.js
@@ -0,0 +1,9 @@
+const person = [
+    new Person ('Juan,', 'Perez'),
+    new Person ('Karla','Lara')
+];
+
+function showPerson(){
+    console.log("Showing person....");
+
+}
---
+++ Js/Person.js
@@ -0,0 +1,20 @@
+class Person{
+    constructor(name, lastName){
+        this._name = name;
+        this._lastName = lastName;
+    }
+
+    get name(){
+        return this._name;
+    }
+    set name(name){
+        this._name = name;
+    }
+
+    get lastName(){
+        this._lastName;
+    }
+    set lastName(lastName){
+        this._lastName = lastName;
+    }
+}
